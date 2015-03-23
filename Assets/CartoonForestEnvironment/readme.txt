ea9f6fda4920523ab38b0b0f882eb4087c07990a
Christian <chrislugram@gmail.com>
2015-03-23 21:44:38 +0100

    First Commit.
    - First step of game
    - First step of GSState System
    - Added Resources

---
+++ Assets/CartoonForestEnvironment/readme.txt
@@ -0,0 +1,6 @@
+Thanks you for buying Cartoon Forest Environment!
+
+Enjoy it and good luck in games development!
+
+This is 1.0 version. 
+If you want to contact me write to doubstract@gmail.com
---
+++ Assets/Scripts/SpeederBike/Bullet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody> ().velocity = this.transform.forward.normalized*50;
+	}
+}
